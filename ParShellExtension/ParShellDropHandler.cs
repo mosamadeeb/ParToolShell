6b5505af315d25076e66b3c6883f2982ea3fee25
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2022-06-22 18:29:05 +0300

    Create ParShellDropHandler
    Only supports adding to existing pars. Since it associates to the extension class, it might be necessary to remove the existing default program for the extension.

---
+++ ParShellExtension/ParShellDropHandler.cs
@@ -0,0 +1,27 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpDropHandler;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using static ParShellExtension.ParShellCommon;
+
+namespace ParShellExtension
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".par")]
+    public class ParShellDropHandler : SharpDropHandler
+    {
+        protected override void DragEnter(DragEventArgs dragEventArgs)
+        {
+            dragEventArgs.Effect = DragDropEffects.Copy;
+        }
+
+        protected override void Drop(DragEventArgs dragEventArgs)
+        {
+            Task.Run(() => ParAdd(Path.GetDirectoryName(SelectedItemPath), DragItems.ToList(), SelectedItemPath, false));
+        }
+    }
+}
