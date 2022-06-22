8305769834109a9b6ee46e3a5e6ca8de75e3eecd
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2022-06-22 18:26:00 +0300

    Reorganize project and implement async

---
+++ ParShellExtension/ParShellCommon.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParShellExtension
+{
+    using Options = ParTool.Options;
+    using ParTool = ParTool.Program;
+
+    public class ParShellCommon
+    {
+        public static async Task ParExtract(string parPath, string outPath, bool recursive)
+        {
+            Options.Extract opt = new Options.Extract
+            {
+                ParArchivePath = parPath,
+                OutputDirectory = outPath,
+                Recursive = recursive,
+            };
+
+            try
+            {
+                ParTool.Extract(opt);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static async Task ParRepack(string inPath, string parPath, bool compress)
+        {
+            Options.Create opt = new Options.Create
+            {
+                InputDirectory = inPath,
+                ParArchivePath = parPath,
+                Compression = compress ? 1 : 0,
+                AlternativeMode = false,
+            };
+
+            if (compress)
+            {
+
+            }
+
+            try
+            {
+                ParTool.Create(opt);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static async Task ParAdd(string inPath, List<string> selected, string parPath, bool compress)
+        {
+            Options.Add opt = new Options.Add
+            {
+                InputParArchivePath = parPath,
+                AddDirectory = inPath,
+                OutputParArchivePath = parPath,
+                Compression = compress ? 1 : 0,
+            };
+
+            try
+            {
+                ParTool.Add(opt, selected.Where(f => File.Exists(f)).ToList(), selected.Where(f => Directory.Exists(f)).ToList());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static async Task ParCreate(string inPath, List<string> selected, string parPath, bool compress)
+        {
+            Options.Create opt = new Options.Create
+            {
+                InputDirectory = inPath,
+                ParArchivePath = parPath,
+                Compression = compress ? 1 : 0,
+                AlternativeMode = false,
+            };
+
+            try
+            {
+                ParTool.Create(opt, selected.Where(f => File.Exists(f)).ToList(), selected.Where(f => Directory.Exists(f)).ToList());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
--- ParShellExtension/ParShellContextMenu.cs
+++ ParShellExtension/ParShellContextMenu.cs
@@ -1,19 +1,19 @@
 ﻿using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
-using System.Linq;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ParShellExt
-{
-    using Options = ParTool.Options;
-    using ParTool = ParTool.Program;
+using static ParShellExtension.ParShellCommon;
 
+namespace ParShellExtension
+{
     [ComVisible(true)]
     [COMServerAssociation(AssociationType.AllFilesAndFolders)]
-    public class ParContextMenu : SharpContextMenu
+    public class ParShellContextMenu : SharpContextMenu
     {
         protected override bool CanShowMenu()
         {
@@ -38,11 +38,11 @@ namespace ParShellExt
                 }
 
                 ToolStripMenuItem extractItem = new ToolStripMenuItem(extractText);
-                extractItem.Click += (sender, args) => ParExtract(selected[0], outPath, false);
+                extractItem.Click += (sender, args) => Task.Run(() => ParExtract(selected[0], outPath, false));
                 parToolItem.DropDownItems.Add(extractItem);
 
                 ToolStripMenuItem extractItemRecursive = new ToolStripMenuItem(extractText + " recursively");
-                extractItemRecursive.Click += (sender, args) => ParExtract(selected[0], outPath, true);
+                extractItemRecursive.Click += (sender, args) => Task.Run(() => ParExtract(selected[0], outPath, true));
                 parToolItem.DropDownItems.Add(extractItemRecursive);
             }
 
@@ -59,11 +59,11 @@ namespace ParShellExt
                 }
 
                 ToolStripMenuItem repackItem = new ToolStripMenuItem(repackText);
-                repackItem.Click += (sender, args) => ParRepack(selected[0], parRepackPath, false);
+                repackItem.Click += (sender, args) => Task.Run(() => ParRepack(selected[0], parRepackPath, false));
                 parToolItem.DropDownItems.Add(repackItem);
 
                 ToolStripMenuItem repackItemCompression = new ToolStripMenuItem(repackText + " with compression");
-                repackItemCompression.Click += (sender, args) => ParRepack(selected[0], parRepackPath, true);
+                repackItemCompression.Click += (sender, args) => Task.Run(() => ParRepack(selected[0], parRepackPath, true));
                 parToolItem.DropDownItems.Add(repackItemCompression);
             }
 
@@ -94,13 +94,13 @@ namespace ParShellExt
 
             if (parExists)
             {
-                addItem.Click += (sender, args) => ParAdd(folderPath, selected, parPath, false);
-                addItemCompression.Click += (sender, args) => ParAdd(folderPath, selected, parPath, true);
+                addItem.Click += (sender, args) => Task.Run(() => ParAdd(folderPath, selected, parPath, false));
+                addItemCompression.Click += (sender, args) => Task.Run(() => ParAdd(folderPath, selected, parPath, true));
             }
             else
             {
-                addItem.Click += (sender, args) => ParCreate(folderPath, selected, parPath, false);
-                addItemCompression.Click += (sender, args) => ParCreate(folderPath, selected, parPath, true);
+                addItem.Click += (sender, args) => Task.Run(() => ParCreate(folderPath, selected, parPath, false));
+                addItemCompression.Click += (sender, args) => Task.Run(() => ParCreate(folderPath, selected, parPath, true));
             }
 
             parToolItem.DropDownItems.Add(addItem);
@@ -108,57 +108,6 @@ namespace ParShellExt
 
             menu.Items.Add(parToolItem);
             return menu;
-        }
-
-        private void ParExtract(string parPath, string outPath, bool recursive)
-        {
-            Options.Extract opt = new Options.Extract
-            {
-                ParArchivePath = parPath,
-                OutputDirectory = outPath,
-                Recursive = recursive,
-            };
-
-            ParTool.Extract(opt);
-        }
-
-        private void ParRepack(string inPath, string parPath, bool compress)
-        {
-            Options.Create opt = new Options.Create
-            {
-                InputDirectory = inPath,
-                ParArchivePath = parPath,
-                Compression = compress ? 1 : 0,
-                AlternativeMode = false,
-            };
-
-            ParTool.Create(opt);
-        }
-
-        private void ParAdd(string inPath, List<string> selected, string parPath, bool compress)
-        {
-            Options.Add opt = new Options.Add
-            {
-                InputParArchivePath = parPath,
-                AddDirectory = inPath,
-                OutputParArchivePath = parPath,
-                Compression = compress ? 1 : 0,
-            };
-
-            ParTool.Add(opt, selected.Where(f => File.Exists(f)).ToList(), selected.Where(f => Directory.Exists(f)).ToList());
-        }
-
-        private void ParCreate(string inPath, List<string> selected, string parPath, bool compress)
-        {
-            Options.Create opt = new Options.Create
-            {
-                InputDirectory = inPath,
-                ParArchivePath = parPath,
-                Compression = compress ? 1 : 0,
-                AlternativeMode = false,
-            };
-
-            ParTool.Create(opt, selected.Where(f => File.Exists(f)).ToList(), selected.Where(f => Directory.Exists(f)).ToList());
         }
     }
 }
